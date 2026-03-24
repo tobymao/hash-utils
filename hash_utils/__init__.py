a404dc2442a0a91e9d2b318ba7206611e34c3fe0
tobymao <toby.mao@gmail.com>
2026-03-23 21:19:49 -0700

    Rename PyPI package to fast-hash-utils

--- hash_utils/__init__.py
+++ hash_utils/__init__.py
@@ -4,5 +4,5 @@ from importlib.metadata import version
 
 from hash_utils._core import dict_hash, shape_hash
 
-__version__ = version("hash-utils")
+__version__ = version("fast-hash-utils")
 __all__ = ["dict_hash", "shape_hash"]
